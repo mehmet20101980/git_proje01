04990ded6d14b7318e3cefde5ba4aafe694175e4
mehmet20101980 <mehmetadg503481@gmail.com>
2020-06-21 21:14:49 +0300

    Ikinci commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Ilk commit from local repository");
+		System.out.println("Ikinci commit from local repository");
+		
 
 	}
 
